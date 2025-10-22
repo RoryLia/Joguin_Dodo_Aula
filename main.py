83bcf30a150a3903f2c5ac95733030f05903b047
Lia Barbosa <136918226+RoryLia@users.noreply.github.com>
2025-10-22 13:47:37 +0000

    Relembrando Python

---
+++ main.py
@@ -0,0 +1,48 @@
+#print ("Hello World")
+
+#print o seu nome
+
+#print ("O meu nome é Lais")
+
+#nome = "SENAI"
+#idade = 83
+#print (nome, idade)
+
+"""a = 10
+b = 5
+soma = a + b
+print (soma)
+
+name = input("Digite o seu nome: ")
+print ("Olá", name)
+
+idade = int(input("Digite a sua idade: "))
+if idade >= 18:
+    print ("Você é de maior!")
+else:
+    print ("Você é de menor!")
+
+fim = 10
+
+
+for numero in range(0, fim):
+  if numero % 2 == 0:
+    print(numero)
+
+def saudacao(nome):
+    print("Bem-vindo", nome)
+
+saudacao("Lucas")
+
+def soma(a, b):
+  resultado = a + b
+  return resultado
+
+resultado_soma = soma(3, 5)
+print(f"A soma é: {resultado_soma}")
+
+import random
+
+cores = ["azul", "verde", "vermelho", "rosa"]
+cor_sorteada = random.choice(cores)
+print ("A cor escolhida é: ", cor_sorteada)"""
